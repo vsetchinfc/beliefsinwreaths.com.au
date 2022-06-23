efaf6816a672bae56139dc0fe0b381b10f4fe207
Vlad <vlad.setchin@gmail.com>
2022-06-23 21:51:00 +1000

    Added chat with us functionality

---
+++ js/tawk.to.js
@@ -0,0 +1,11 @@
+var Tawk_API = Tawk_API || {},
+  Tawk_LoadStart = new Date();
+(function () {
+  var s1 = document.createElement("script"),
+    s0 = document.getElementsByTagName("script")[0];
+  s1.async = true;
+  s1.src = "https://embed.tawk.to/62b3ae9b7b967b11799608c8/1g66u44po";
+  s1.charset = "UTF-8";
+  s1.setAttribute("crossorigin", "*");
+  s0.parentNode.insertBefore(s1, s0);
+})();
