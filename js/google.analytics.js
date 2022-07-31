b5e19963bccb8d2e212c2bbf15609b74ac543f19
Vlad <vlad.setchin@gmail.com>
2022-07-31 22:00:26 +1000

    Updated ga code

--- js/google.analytics.js
+++ js/google.analytics.js
@@ -2,7 +2,6 @@ window.dataLayer = window.dataLayer || [];
 function gtag() {
   dataLayer.push(arguments);
 }
-
 gtag("js", new Date());
 
-gtag("config", "G-4R2V0MR1SQ");
+gtag("config", "UA-207409102-2");
